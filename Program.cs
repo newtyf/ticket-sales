507fc8a46a68ad83ae884e0b9dae98da09bb44bf
newt <axelskam.123@outlook.es>
2022-10-16 15:08:48 -0500

    update indeces of data tickets json

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using Spectre.Console;
 using Newtonsoft.Json;
+using System.IO;
 
 FigletText titleProyect = new FigletText("Simulación Venta de Pasajes").LeftAligned().Color(Color.Blue);
 AnsiConsole.Write(titleProyect);
@@ -8,12 +9,11 @@ AnsiConsole.Write(titleProyect);
 // get data boletos
 string pathData = @"C:\Users\AXEL\Desktop\UNIVERSITY\UPN\C#\Fundamentos de Algoritmos\T4\SimulacionVentaPasajes\data_tickets.json";
 List<Boleto>? tickets;
-using (StreamReader jsonStream = File.OpenText(pathData))
-{
-    var json = jsonStream.ReadToEnd();
-    tickets = JsonConvert.DeserializeObject<List<Boleto>>(json);
-}
-Console.WriteLine(tickets?[0].Id);
+StreamReader jsonStream = File.OpenText(pathData);
+var json = jsonStream.ReadToEnd();
+tickets = JsonConvert.DeserializeObject<List<Boleto>>(json);
+
+Console.WriteLine(tickets?[1].DateOfExitArrival);
 
 Console.ReadLine();
 
