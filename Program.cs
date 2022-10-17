028b2a19668f68f061516e76c2a812b03af665d1
newt <axelskam.123@outlook.es>
2022-10-17 09:29:01 -0500

    reformat style code

--- Program.cs
+++ Program.cs
@@ -7,11 +7,11 @@ AnsiConsole.Write(titleProyect);
 
 
 // get data boletos
-string pathData = @"C:\Users\AXEL\Desktop\UNIVERSITY\UPN\C#\Fundamentos de Algoritmos\T4\SimulacionVentaPasajes\data_tickets.json";
-List<Boleto>? tickets;
+string pathData =
+    @"C:\Users\AXEL\Desktop\UNIVERSITY\UPN\C#\Fundamentos de Algoritmos\T4\SimulacionVentaPasajes\data_tickets.json";
 StreamReader jsonStream = File.OpenText(pathData);
 var json = jsonStream.ReadToEnd();
-tickets = JsonConvert.DeserializeObject<List<Boleto>>(json);
+List<Boleto>? tickets = JsonConvert.DeserializeObject<List<Boleto>>(json);
 
 Console.WriteLine(tickets?[1].DateOfExitArrival);
 
