6306108e9ccf265436363e3af4d20112ee841ac8
newt <axelskam.123@outlook.es>
2022-10-17 11:05:55 -0500

    add comment title

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using Spectre.Console;
 using Newtonsoft.Json;
 
+// title font
 var font = FigletFont.Load(Path.Combine(Directory.GetParent("../../../").ToString(), "slant.flf"));
 FigletText titleProyect = new FigletText(font, "Venta de Pasajes").LeftAligned().Color(Color.Blue);
 AnsiConsole.Write(titleProyect);
